55b5e98eb7cf53639052128b21050f00a44a4a68
VT7org <glitchnaxusofficial@gmail.com>
2025-06-28 14:56:49 +0530

    .

--- plugins/billa.py
+++ plugins/billa.py
@@ -12,13 +12,13 @@ from pyrogram.types import (
 
 close_keyboard = InlineKeyboardMarkup(
     [
-        [InlineKeyboardButton(text="🔄 **𝗔𝘁𝘂𝗮𝗹𝗶𝘇𝗮𝗿**", callback_data="refresh_cat")],
-        [InlineKeyboardButton(text="❌ **𝗙𝗲𝗰𝗵𝗮𝗿**", callback_data="close")],
+        [InlineKeyboardButton(text="🔄 **Refresh**", callback_data="refresh_cat")],
+        [InlineKeyboardButton(text="❌ **Close**", callback_data="close")],
     ]
 )
 
 
-@app.on_message(filters.command("cat") & ~BANNED_USERS)
+@app.on_message(filters.command("billa") & ~BANNED_USERS)
 async def cat(_client: Client, message: Message):
     r = requests.get("https://api.thecatapi.com/v1/images/search")
     if r.status_code == 200:
@@ -26,12 +26,12 @@ async def cat(_client: Client, message: Message):
         cat_url = data[0]["url"]
         if cat_url.endswith(".gif"):
             await message.reply_animation(
-                cat_url, caption="🐱 **𝗠𝗲𝗼𝘄**", reply_markup=close_keyboard
+                cat_url, caption="🐱 **Billa**", reply_markup=close_keyboard
             )
         else:
-            await message.reply_photo(cat_url, caption="🐱 **𝗠𝗲𝗼𝘄**", reply_markup=close_keyboard)
+            await message.reply_photo(cat_url, caption="🐱 **Meow**", reply_markup=close_keyboard)
     else:
-        await message.reply_text("🙀 **𝗙𝗮𝗹𝗵𝗮 𝗮𝗼 𝗯𝘂𝘀𝗰𝗮𝗿 𝗳𝗼𝘁𝗼 𝗱𝗲 𝗴𝗮𝘁𝗼!**")
+        await message.reply_text("🙀 **Failed to fetch photo of billa!**")
 
 
 @app.on_callback_query(filters.regex("refresh_cat") & ~BANNED_USERS)
@@ -42,12 +42,12 @@ async def refresh_cat(_client: Client, callback_query: CallbackQuery):
         cat_url = data[0]["url"]
         if cat_url.endswith(".gif"):
             await callback_query.edit_message_animation(
-                cat_url, caption="🐱 **𝗠𝗲𝗼𝘄**", reply_markup=close_keyboard
+                cat_url, caption="🐱 **Billa**", reply_markup=close_keyboard
             )
         else:
             await callback_query.edit_message_media(
-                InputMediaPhoto(media=cat_url, caption="🐱 **𝗠𝗲𝗼𝘄**"),
+                InputMediaPhoto(media=cat_url, caption="🐱 **Billa**"),
                 reply_markup=close_keyboard,
             )
     else:
-        await callback_query.edit_message_text("🙀 **𝗙𝗮𝗹𝗵𝗮 𝗮𝗼 𝗮𝘁𝘂𝗮𝗹𝗶𝘇𝗮𝗿 𝗮 𝗳𝗼𝘁𝗼 𝗱𝗼 𝗴𝗮𝘁𝗼!**")
+        await callback_query.edit_message_text("🙀 **Failed to Get & refresh photo of Billa!**")
